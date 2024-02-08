88d9714aad247e297546cf1fdb29bf03f5d8530f
Rudraditya Thakur <moskva@Rudradityas-MacBook-Air.local>
2024-02-08 06:30:46 +0530

    changed about page col

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const AboutPage: React.FC = () => {
   return (
-    <div className="max-w-4xl mx-auto py-8 px-4 sm:px-6 lg:px-8 mt-16">
+    <div className="max-w-4xl mx-auto py-8 px-4 sm:px-6 lg:px-8 mt-16 text-[#569199]">
       <div className="text-center">
         <h1 className="text-3xl font-extrabold text-gray-900">About Medmind Innovision</h1>
         <p className="mt-4 text-lg text-gray-600">
